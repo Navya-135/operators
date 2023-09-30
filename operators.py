6d307d6d8b4285e3f0d0e214f8ae066c3970d10a
Navya-135 <143674020+Navya-135@users.noreply.github.com>
2023-09-30 14:42:05 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,211 @@
+#operators
+
+#arthematic opeators(+,-,*,/,%,//)
+#addition
+
+a = 9
+b = 10
+print(a+b)
+
+#subtraction
+
+a = 10
+b = 20
+c = 15
+print(a-b)
+print(b-c)
+print(a-c)
+
+#multipication
+
+a = 4
+b = 3
+c = 4
+print(a*b)
+print(b*c)
+print(a*c)
+
+#division
+
+a = 12
+b = 6
+print(a/b)
+
+#Module
+
+a = 12
+b = 34
+print(a%b)
+
+#floor division
+
+a = 12
+b = 6
+c = 12
+print(a//b)
+print(b//c)
+
+#Assignment operator
+#equal
+
+a = 10
+b = 12
+print(a)
+
+#+ equal
+
+a = 8
+a += 4
+print(a)
+
+#- equal
+
+a = 6
+a -= 3
+print(a)
+
+#* equal
+
+a = 4
+a *= 4
+print(a)
+
+#% equal
+
+a = 2
+a %= 3
+print(a)
+
+#/ equal
+
+a = 8
+a /= 4
+print(a)
+
+#// equal
+
+a = 12
+a //= 6
+print(a)
+
+#logical operators
+
+#and
+
+a = 6
+b = 5
+c = 10
+print(a<b and a>b and a<c)
+
+#or
+
+a = 4
+b = 6
+c = 4
+print(a<c or a<b or b>a)
+
+#not
+
+a = 4
+b = 6
+c = 4
+print(not(a<c or a<b or b>a))
+
+
+#camparision operators
+# ==
+
+a = 50
+b = 50
+print(a==b)
+
+# <=
+
+a = 10
+b = 5
+print(a<b)
+
+# >=
+
+a = 10
+b = 9
+print(a>b)
+
+# !=
+
+a = 60
+b = 6
+print(a!=b)
+
+#less than
+
+a = 9
+b = 5
+print(a<b)
+
+#greater than
+
+a = 10
+b = 8
+print(a>b)
+
+#bitwise operators
+#and(&)
+
+a = 10
+b = 12
+print(a&b)
+
+#or(|)
+
+a = 4
+b = 3
+print(a|b)
+
+#xor(^)
+
+a = 12
+b = 6
+print(a^b)
+
+#Leftshift operator
+
+print(8>>2)
+
+#rightshift operator
+
+print(2<<1)
+
+#special operators
+# a.Identity operator
+
+#is
+
+a=10
+b=10
+print(a is b)
+
+#is not
+
+a=10
+b=40
+print(a is not b)
+
+# b.Membership operators
+
+#in
+
+a="navya@gmail.com"
+print("@" in a)
+
+#not in
+
+a=["Navya","Rajesh","Mahalya"]
+name="Aravind"
+print(name not in a)
+
+a=["Navya","Rajesh","Mahalya"]
+name="Mahalya"
+print(name not in a)
+
+
